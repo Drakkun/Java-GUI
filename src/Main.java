121c0c6c37af1bb033f6d27f95882a12c541a1b2
Sam <samies2012@gmail.com>
2017-11-15 14:21:08 -0600

    Creates java breakout project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Frame f = new Frame("Example 1");
+        f.show();
+    }
+}
